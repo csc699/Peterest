3f94f60d7ff978286e2e46a8f1f18eafc2391a29
russ27 <rpineda2@mail.sfsu.edu>
2019-04-14 22:50:49 -0700

    Friend tableViewController and friend search viewController

--- Peterest/Peterest/FriendsViewController.swift
+++ Peterest/Peterest/FriendsViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class FriendsViewController: UIViewController {
+class FriendsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text =  "friend: \(indexPath.row)"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         // Do any additional setup after loading the view.
+        
     }
     
 
