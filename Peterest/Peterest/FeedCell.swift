7f4129bbb91ce876a42f4b7d30b8941d694949af
stephaniesantana <stephycsantana@gmail.com>
2019-05-01 00:10:31 -0700

    phase 1 of collection view feed

---
+++ Peterest/Peterest/FeedCell.swift
@@ -0,0 +1,20 @@
+//
+//  FeedCell.swift
+//  Peterest
+//
+//  Created by Stephanie Santana on 4/30/19.
+//  Copyright © 2019 Yingying Chen. All rights reserved.
+//
+
+import UIKit
+
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
